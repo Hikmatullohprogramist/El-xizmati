6bd1b693ff2ab3cb387c4b928381daed3ca4fd32
rustamjon <mullaqoziyev.rustamjon@gmail.com>
2023-11-09 11:45:04 +0500

    notification_setting_page added

--- lib/presentation/home/profile_dashboard/features/setting/features/notification_settings/notification_setting_page.dart
+++ lib/presentation/home/profile_dashboard/features/setting/features/notification_settings/notification_setting_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:onlinebozor/common/base/base_page.dart';
 import 'package:onlinebozor/common/colors/color_extension.dart';
 import 'package:onlinebozor/common/extensions/text_extensions.dart';
+import 'package:onlinebozor/common/widgets/common_button.dart';
 import 'package:onlinebozor/presentation/home/profile_dashboard/features/setting/features/notification_settings/cubit/notification_setting_cubit.dart';
 
 import '../../../../../../../common/gen/assets/assets.gen.dart';
@@ -15,6 +16,7 @@ class NotificationSettingPage extends BasePage<NotificationSettingCubit,
   @override
   Widget builder(BuildContext context, NotificationSettingBuildable state) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: 'Способ получения уведомления'
@@ -28,23 +30,105 @@ class NotificationSettingPage extends BasePage<NotificationSettingCubit,
           onPressed: () => context.router.pop(),
         ),
       ),
-      body: Column(
-        children: [
-          Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                children: [
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            SizedBox(height: 16),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                side: BorderSide(
+                  color: Color(0xFFDFE2E9),
+                ),
+              ),
+              onPressed: () {},
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Row(children: [
                   Container(
+                    width: 32,
+                    height: 32,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
                         color: Color(0xFF5C6AC3)),
-                  )
-                ],
-              )),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-          )
-        ],
+                    child: Center(child: Assets.images.icSms.svg()),
+                  ),
+                  SizedBox(width: 16),
+                  "СМС собщение".w(600).s(14).c(Color(0xFF41455E))
+                ]),
+              ),
+            ),
+            SizedBox(height: 10),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                side: BorderSide(
+                  color: Color(0xFFDFE2E9),
+                ),
+              ),
+              onPressed: () {},
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Row(children: [
+                  Container(
+                    width: 32,
+                    height: 32,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF00A4DD)),
+                    child: Center(child: Assets.images.icSms.svg()),
+                  ),
+                  SizedBox(width: 16),
+                  "Телеграм бот".w(600).s(14).c(Color(0xFF41455E))
+                ]),
+              ),
+            ),
+            SizedBox(height: 10),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                side: BorderSide(
+                  color: Color(0xFFDFE2E9),
+                ),
+              ),
+              onPressed: () {},
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Row(children: [
+                  Container(
+                    width: 32,
+                    height: 32,
+                    decoration: BoxDecoration(
+                      border:Border.all(color: Color(0xFFDFE2E9), width: 1) ,
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white),
+                    child: Center(child: Assets.images.icSms.svg()),
+                  ),
+                  SizedBox(width: 16),
+                  "Через Эл. почту".w(600).s(14).c(Color(0xFF41455E))
+                ]),
+              ),
+            ),
+            Spacer(),
+            SizedBox(
+              width: double.infinity,
+              child: CommonButton(
+                  onPressed: () {},
+                  child: "Сохранить".w(600).s(14).c(Colors.white)),
+            ),
+            SizedBox(height: 16)
+          ],
+        ),
       ),
     );
   }
