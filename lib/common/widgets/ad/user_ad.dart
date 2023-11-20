9d8edf89a49fab01917fa326e7b58688e2fa3df7
rustamjon <mullaqoziyev.rustamjon@gmail.com>
2023-11-20 22:27:00 +0500

    user ad  detail screen added.

--- lib/common/widgets/ad/user_ad.dart
+++ lib/common/widgets/ad/user_ad.dart
@@ -19,146 +19,147 @@ class UserAd extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+        onTap: onClick,
         child: Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        children: [
-          Row(
+          padding: const EdgeInsets.all(16),
+          child: Column(
             children: [
-              Container(
-                  width: 80,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(6),
-                    border: Border.all(width: 0.50, color: Color(0xFFE5E9F3)),
-                    color: Color(0xFFF6F7FC),
-                  ),
-                  child: CachedNetworkImage(
-                    imageUrl: "${Constants.baseUrlForImage}",
-                    imageBuilder: (context, imageProvider) => Container(
+              Row(
+                children: [
+                  Container(
+                      width: 80,
+                      height: 100,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(6),
-                        image: DecorationImage(
-                            image: imageProvider,
-                            fit: BoxFit.cover,
-                            colorFilter: ColorFilter.mode(
-                                Colors.white, BlendMode.colorBurn)),
+                        border: Border.all(width: 0.50, color: Color(0xFFE5E9F3)),
+                        color: Color(0xFFF6F7FC),
                       ),
-                    ),
-                    placeholder: (context, url) => Center(),
-                    errorWidget: (context, url, error) =>
-                        Center(child: Icon(Icons.error)),
-                  )),
-              SizedBox(width: 12),
-              Expanded(
-                  child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                        child:
-                            "Электромобиль Volkswagen ID4 Pro Crozz Full varianti"
-                                .w(600)
-                                .s(14)
-                                .c(Color(0xFF41455E))
-                                .copyWith(
+                      child: CachedNetworkImage(
+                        imageUrl: "${Constants.baseUrlForImage}",
+                        imageBuilder: (context, imageProvider) => Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6),
+                            image: DecorationImage(
+                                image: imageProvider,
+                                fit: BoxFit.cover,
+                                colorFilter: ColorFilter.mode(
+                                    Colors.white, BlendMode.colorBurn)),
+                          ),
+                        ),
+                        placeholder: (context, url) => Center(),
+                        errorWidget: (context, url, error) =>
+                            Center(child: Icon(Icons.error)),
+                      )),
+                  SizedBox(width: 12),
+                  Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Flexible(
+                                child:
+                                "Электромобиль Volkswagen ID4 Pro Crozz Full varianti"
+                                    .w(600)
+                                    .s(14)
+                                    .c(Color(0xFF41455E))
+                                    .copyWith(
                                     maxLines: 2,
                                     overflow: TextOverflow.ellipsis),
-                      ),
-                      IconButton(
-                          onPressed: () {
-                            editClick();
-                          },
-                          icon: Assets.images.icMoreVert
-                              .svg(width: 24, height: 24)),
-                    ],
-                  ),
-                  SizedBox(height: 27),
-                  Row(
-                    children: [
-                      "Остаток:".w(400).s(12).c(Color(0xFF9EABBE)),
-                      SizedBox(width: 2),
-                      "2".w(500).s(12).c(Color(0xFF41455E)),
-                      Spacer(),
-                      "473 769 560 сум".w(700).s(12).c(Color(0xFF5C6AC3))
-                    ],
-                  ),
-                  SizedBox(height: 8),
-                  "Транспорт - Легковые автомобили"
-                      .w(400)
-                      .s(12)
-                      .c(Color(0xFF9EABBE))
-                      .copyWith(maxLines: 1, overflow: TextOverflow.ellipsis)
+                              ),
+                              IconButton(
+                                  onPressed: () {
+                                    editClick();
+                                  },
+                                  icon: Assets.images.icMoreVert
+                                      .svg(width: 24, height: 24)),
+                            ],
+                          ),
+                          SizedBox(height: 27),
+                          Row(
+                            children: [
+                              "Остаток:".w(400).s(12).c(Color(0xFF9EABBE)),
+                              SizedBox(width: 2),
+                              "2".w(500).s(12).c(Color(0xFF41455E)),
+                              Spacer(),
+                              "473 769 560 сум".w(700).s(12).c(Color(0xFF5C6AC3))
+                            ],
+                          ),
+                          SizedBox(height: 8),
+                          "Транспорт - Легковые автомобили"
+                              .w(400)
+                              .s(12)
+                              .c(Color(0xFF9EABBE))
+                              .copyWith(maxLines: 1, overflow: TextOverflow.ellipsis)
+                        ],
+                      ))
                 ],
-              ))
-            ],
-          ),
-          SizedBox(height: 12),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(6),
-                  border: Border.all(width: 1, color: Color(0xFFDADDE5)),
-                ),
-                child:
-                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  Assets.images.icEye.svg(
-                      height: 12, width: 12, color: context.colors.iconGrey),
-                  SizedBox(width: 8),
-                  "136".w(600).s(10).c(Color(0xFF41455E))
-                ]),
               ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(6),
-                  border: Border.all(width: 1, color: Color(0xFFDADDE5)),
-                ),
-                child:
+              SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      border: Border.all(width: 1, color: Color(0xFFDADDE5)),
+                    ),
+                    child:
                     Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  Assets.images.icLiked.svg(
-                      height: 12, width: 12, color: context.colors.iconGrey),
-                  SizedBox(width: 8),
-                  "136".w(600).s(10).c(Color(0xFF41455E))
-                ]),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(6),
-                  border: Border.all(width: 1, color: Color(0xFFDADDE5)),
-                ),
-                child:
+                      Assets.images.icEye.svg(
+                          height: 12, width: 12, color: context.colors.iconGrey),
+                      SizedBox(width: 8),
+                      "136".w(600).s(10).c(Color(0xFF41455E))
+                    ]),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      border: Border.all(width: 1, color: Color(0xFFDADDE5)),
+                    ),
+                    child:
                     Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  Assets.images.icCall.svg(
-                      height: 12, width: 12, color: context.colors.iconGrey),
-                  SizedBox(width: 8),
-                  "136".w(600).s(10).c(Color(0xFF41455E))
-                ]),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(6),
-                  border: Border.all(width: 1, color: Color(0xFFDADDE5)),
-                ),
-                child:
+                      Assets.images.icLiked.svg(
+                          height: 12, width: 12, color: context.colors.iconGrey),
+                      SizedBox(width: 8),
+                      "136".w(600).s(10).c(Color(0xFF41455E))
+                    ]),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      border: Border.all(width: 1, color: Color(0xFFDADDE5)),
+                    ),
+                    child:
                     Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  Assets.images.icSms.svg(
-                      height: 12, width: 12, color: context.colors.iconGrey),
-                  SizedBox(width: 8),
-                  "136".w(600).s(10).c(Color(0xFF41455E))
-                ]),
+                      Assets.images.icCall.svg(
+                          height: 12, width: 12, color: context.colors.iconGrey),
+                      SizedBox(width: 8),
+                      "136".w(600).s(10).c(Color(0xFF41455E))
+                    ]),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      border: Border.all(width: 1, color: Color(0xFFDADDE5)),
+                    ),
+                    child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                      Assets.images.icSms.svg(
+                          height: 12, width: 12, color: context.colors.iconGrey),
+                      SizedBox(width: 8),
+                      "136".w(600).s(10).c(Color(0xFF41455E))
+                    ]),
+                  )
+                ],
               )
             ],
-          )
-        ],
-      ),
-    ));
+          ),
+        ));
   }
 }
--- lib/presentation/ad/user_ad_detail/user_ad_detail.dart
+++ lib/presentation/ad/user_ad_detail/user_ad_detail.dart
@@ -1,8 +1,15 @@
-import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:onlinebozor/common/base/base_page.dart';
+import 'package:onlinebozor/common/colors/color_extension.dart';
 import 'package:onlinebozor/common/extensions/text_extensions.dart';
+import 'package:onlinebozor/common/gen/assets/assets.gen.dart';
+import 'package:onlinebozor/common/widgets/app_diverder.dart';
+import 'package:onlinebozor/common/widgets/common_button.dart';
 import 'package:onlinebozor/presentation/ad/user_ad_detail/cubit/user_ad_detail_cubit.dart';
+
+import '../../../common/constants.dart';
+import '../../../common/widgets/app_image_widget.dart';
 
 @RoutePage()
 class UserAdDetailPage extends BasePage<UserAdDetailCubit,
@@ -12,7 +19,173 @@ class UserAdDetailPage extends BasePage<UserAdDetailCubit,
   @override
   Widget builder(BuildContext context, UserAdDetailBuildable state) {
     return Scaffold(
-      body: Center(child: "User Ad Detail".w(500).s(16)),
+      appBar: AppBar(
+          title: "ID12345677".w(500),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          elevation: 1,
+          leading: IconButton(
+              onPressed: () => context.router.pop(),
+              icon: Assets.images.icArrowLeft.svg(height: 24, width: 24)),
+          actions: [
+            Padding(
+                padding: EdgeInsets.all(4),
+                child: IconButton(
+                  icon: Assets.images.icMoreVert.svg(height: 24, width: 24),
+                  onPressed: () {},
+                ))
+          ]),
+      body: ListView(
+          shrinkWrap: true,
+          physics: BouncingScrollPhysics(),
+          children: [
+            AppImageWidget(
+                images: (List.empty(growable: true))
+                    .map((e) => "${Constants.baseUrlForImage}${e.image}")
+                    .toList()),
+            SizedBox(height: 24),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              child: "Электромобиль Volkswagen ID4 Pro Crozz Full"
+                  .w(600)
+                  .s(14)
+                  .c(Color(0xFF41455E))
+                  .copyWith(maxLines: 3, overflow: TextOverflow.ellipsis),
+            ),
+            SizedBox(height: 24),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                "Остаток:".w(400).s(12).c(Color(0xFF9EABBE)),
+                SizedBox(width: 6),
+                "2".s(12).c(Color(0xFF41455E)).w(500)
+              ]),
+            ),
+            SizedBox(height: 6),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: "Транспорт - Легковые автомобили"
+                    .w(400)
+                    .s(12)
+                    .c(Color(0xFF9EABBE))
+                    .copyWith(maxLines: 2, overflow: TextOverflow.ellipsis)),
+            SizedBox(height: 24),
+            AppDivider(height: 1),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 22),
+              child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                "473 769 560 сум".w(700).s(16).c(Color(0xFF5C6AC3))
+              ]),
+            ),
+            AppDivider(height: 1),
+            SizedBox(height: 16),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Row(children: [
+                Assets.images.icCalendar.svg(width: 24, height: 24),
+                SizedBox(width: 8),
+                "02.02.2023 - 04.03.2023".w(500).s(12).c(Color(0xFF9EABBE)),
+              ]),
+            ),
+            SizedBox(height: 16),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                Assets.images.icLocation.svg(height: 24, width: 24),
+                SizedBox(width: 6),
+                "Ташкент, Юнусабадский район".w(400).s(12).c(Color(0xFF9EABBE))
+              ]),
+            ),
+            SizedBox(height: 16),
+            AppDivider(height: 1),
+            Container(
+              margin: EdgeInsets.all(16),
+              child: CommonButton(
+                onPressed: () {},
+                child:
+                    "Посмотреть статистику".w(500).s(12).c(Color(0xFF41455E)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      border: Border.all(width: 1, color: Color(0xFFDADDE5)),
+                    ),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Assets.images.icEye.svg(
+                              height: 12,
+                              width: 12,
+                              color: context.colors.iconGrey),
+                          SizedBox(width: 8),
+                          "136".w(600).s(10).c(Color(0xFF41455E))
+                        ]),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      border: Border.all(width: 1, color: Color(0xFFDADDE5)),
+                    ),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Assets.images.icLiked.svg(
+                              height: 12,
+                              width: 12,
+                              color: context.colors.iconGrey),
+                          SizedBox(width: 8),
+                          "136".w(600).s(10).c(Color(0xFF41455E))
+                        ]),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      border: Border.all(width: 1, color: Color(0xFFDADDE5)),
+                    ),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Assets.images.icCall.svg(
+                              height: 12,
+                              width: 12,
+                              color: context.colors.iconGrey),
+                          SizedBox(width: 8),
+                          "136".w(600).s(10).c(Color(0xFF41455E))
+                        ]),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      border: Border.all(width: 1, color: Color(0xFFDADDE5)),
+                    ),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Assets.images.icSms.svg(
+                              height: 12,
+                              width: 12,
+                              color: context.colors.iconGrey),
+                          SizedBox(width: 8),
+                          "136".w(600).s(10).c(Color(0xFF41455E))
+                        ]),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 24)
+          ]),
     );
   }
 }
