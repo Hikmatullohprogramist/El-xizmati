f27c4087e652d4906da6824fdf746ef8db8d9932
kebrorba <abror.esonaliyev@gmail.com>
2024-02-14 15:55:51 +0500

    vibrator package is added

---
+++ lib/common/vibrator/vibrator_extension.dart
@@ -0,0 +1,13 @@
+import 'package:vibration/vibration.dart';
+
+Future<void> vibrateByTactile() async {
+  if (await Vibration.hasCustomVibrationsSupport() == true) {
+    Vibration.vibrate(duration: 5, amplitude: 40);
+  }
+}
+
+Future<void> vibrate() async {
+  if (await Vibration.hasVibrator() == true) {
+    Vibration.vibrate(duration: 50); // Vibrate for 50 milliseconds
+  }
+}
