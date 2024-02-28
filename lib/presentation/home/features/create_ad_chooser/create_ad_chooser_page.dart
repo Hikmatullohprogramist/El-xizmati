cacaaf516c3aabe450b66f12f4c7e8562ee7e1bd
Nurali Rasuljonov <rasuljonovnurali04@gmail.com>
2024-02-28 18:55:08 +0500

    CartPage's item fixed enable size

--- lib/presentation/home/features/create_ad_chooser/create_ad_chooser_page.dart
+++ lib/presentation/home/features/create_ad_chooser/create_ad_chooser_page.dart
@@ -30,21 +30,12 @@ class CreateAdChooserPage extends BasePage<CreateAdChooserCubit, CreateAdChooser
         physics: BouncingScrollPhysics(),
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: [
-             Visibility(
-               visible: state.isLogin,
-                 child: Column(
-                   children: [
-                     _buildSaleBlock(context),
-                     _buildBuyBlock(context),
-                   ],
-                 )
-             ),
-            Visibility(
-                visible: !state.isLogin,
-                child: _buildDirectToRegister(context)
-            ),
-
+          children: state.isLogin
+              ? [
+            _buildSaleBlock(context),
+            _buildBuyBlock(context),
+          ] : [
+            _buildDirectToRegister(context),
           ],
         ),
       ),
@@ -185,19 +176,19 @@ class CreateAdChooserPage extends BasePage<CreateAdChooserCubit, CreateAdChooser
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 36),
       child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-        SizedBox(height: 150),
+        SizedBox(height: 170),
         Assets.images.pngImages.adEmpty.image(),
         SizedBox(height: 48),
-        Strings.authRecommentTitle.w(500).s(16).c(Color(0xFF41455E)).copyWith(
+        Strings.authRecommentTitle.w(500).s(20).c(Color(0xFF41455E)).copyWith(
             maxLines: 2,
             overflow: TextOverflow.ellipsis,
             textAlign: TextAlign.start),
         SizedBox(height: 12),
-        Strings.authRecommentDesc.w(400).s(12).c(Color(0xFF41455E)).copyWith(
+        Strings.authRecommentDesc.w(400).s(18).c(Color(0xFF41455E)).copyWith(
           maxLines: 2,
           textAlign: TextAlign.center,
         ),
-        SizedBox(height: 100),
+        SizedBox(height: 130),
         SizedBox(
             width: double.maxFinite,
             child: CommonButton(
