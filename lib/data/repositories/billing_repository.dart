d20bf8c8d0d1788482776ab43e57c718fd058f24
kebrorba <abror.esonaliyev@gmail.com>
2024-06-14 16:33:16 +0500

    fixed: checking identity verifying state2

--- lib/data/repositories/billing_repository.dart
+++ lib/data/repositories/billing_repository.dart
@@ -2,6 +2,7 @@ import 'package:collection/collection.dart';
 import 'package:onlinebozor/data/datasource/network/responses/billing/billing_transaction_response.dart';
 import 'package:onlinebozor/data/datasource/network/services/payment_service.dart';
 import 'package:onlinebozor/data/datasource/preference/auth_preferences.dart';
+import 'package:onlinebozor/data/datasource/preference/user_preferences.dart';
 import 'package:onlinebozor/data/error/app_locale_exception.dart';
 import 'package:onlinebozor/data/mappers/billing_mapper.dart';
 import 'package:onlinebozor/domain/models/billing/billing_transaction.dart';
@@ -9,10 +10,12 @@ import 'package:onlinebozor/domain/models/billing/billing_transaction.dart';
 class BillingRepository {
   final AuthPreferences _authPreferences;
   final PaymentService _paymentService;
+  final UserPreferences _userPreferences;
 
   BillingRepository(
     this._authPreferences,
     this._paymentService,
+    this._userPreferences,
   );
 
   Future<List<BillingTransaction>> getPaymentTransactions({
