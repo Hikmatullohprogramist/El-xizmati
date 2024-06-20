3647e627e458a66ca4e3ee11e93ef354feb35f7b
kebrorba <abror.esonaliyev@gmail.com>
2024-06-20 07:53:43 +0500

    changed: search_input_field removed search-image-button

--- lib/presentation/widgets/search/search_input_field.dart
+++ lib/presentation/widgets/search/search_input_field.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:onlinebozor/core/gen/assets/assets.gen.dart';
 import 'package:onlinebozor/core/gen/localization/strings.dart';
 import 'package:onlinebozor/presentation/support/extensions/color_extension.dart';
 
@@ -33,27 +34,66 @@ class _SearchInputFieldState extends State<SearchInputField> {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      autofocus: true,
-      controller: _controller,
-      onChanged: (value){
-        _onSearchChanged(value);
-      },
-      onSubmitted: (value) {
-        _onSearchChanged(value);
-      },
-      style: TextStyle(
-        color: context.textPrimary,
-        fontSize: 14,
-      ),
-      decoration: InputDecoration.collapsed(
-        hintText: Strings.searchHintCategoryAndProducts,
-        hintStyle: TextStyle(
-          color: context.textSecondary,
-          fontSize: 12,
+    return Container(
+      margin: EdgeInsets.only(left: 64, top: 8, bottom: 8, right: 16),
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      decoration: ShapeDecoration(
+        color: context.inputBackgroundColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(6),
         ),
       ),
-      keyboardType: TextInputType.text,
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              autofocus: true,
+              controller: _controller,
+              onChanged: (value) {
+                _onSearchChanged(value);
+              },
+              onSubmitted: (value) {
+                // _onSearchChanged(value);
+              },
+              style: TextStyle(
+                color: context.textPrimary,
+                fontSize: 14,
+              ),
+              decoration: InputDecoration.collapsed(
+                hintText: Strings.searchHintCategoryAndProducts,
+                hintStyle: TextStyle(
+                  color: context.textSecondary,
+                  fontSize: 12,
+                ),
+              ),
+              keyboardType: TextInputType.text,
+            ),
+          ),
+          // Material(
+          //   color: Colors.transparent,
+          //   child: InkWell(
+          //     borderRadius: BorderRadius.circular(12),
+          //     onTap: () {
+          //       _onSearchChanged(_controller.text);
+          //     },
+          //     child: Assets.images.iconSearch
+          //         .svg(color: Colors.blueAccent, width: 24, height: 24),
+          //   ),
+          // ),
+          // SizedBox(width: 8),
+          Material(
+            color: Colors.transparent,
+            child: InkWell(
+              borderRadius: BorderRadius.circular(12),
+              onTap: () {
+                _controller.clear();
+                _onSearchChanged(_controller.text);
+              },
+              child: Assets.images.icClose.svg(width: 24, height: 24),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
