41a096c117aafa2ebf1f05105d5ec55dc6538a7a
kebrorba <abror.esonaliyev@gmail.com>
2024-07-08 20:50:53 +0500

    replaced: app bar action replaced with default tab controller

--- lib/presentation/features/home/features/profile/features/user_orders/user_orders_page.dart
+++ lib/presentation/features/home/features/profile/features/user_orders/user_orders_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:logger/logger.dart';
 import 'package:onlinebozor/core/extensions/text_extensions.dart';
 import 'package:onlinebozor/core/gen/assets/assets.gen.dart';
 import 'package:onlinebozor/core/gen/localization/strings.dart';
@@ -22,6 +23,7 @@ class UserOrdersPage
 
   @override
   void onWidgetCreated(BuildContext context) {
+    Logger().w("onWidgetCreated orderType = $orderType");
     cubit(context).setInitialParams(orderType);
   }
 
@@ -31,7 +33,8 @@ class UserOrdersPage
       switch (event.type) {
         case UserOrdersEventType.onOrderTypeChange:
           context.router.pop();
-          context.router.push(
+          Logger().w("onEventEmitted orderType = ${cubit(context).states.orderType}");
+          context.router.replace(
             UserOrdersRoute(orderType: cubit(context).states.orderType),
           );
       }
@@ -40,92 +43,10 @@ class UserOrdersPage
 
   @override
   Widget onWidgetBuild(BuildContext context, UserOrdersState state) {
-    // return AutoTabsRouter.tabBar(
-    //   physics: BouncingScrollPhysics(),
-    //   routes: [
-    //     UserOrderListRoute(
-    //       type: state.orderType,
-    //       status: UserOrderStatus.ALL,
-    //     ),
-    //     UserOrderListRoute(
-    //       type: state.orderType,
-    //       status: UserOrderStatus.WAIT,
-    //     ),
-    //     UserOrderListRoute(
-    //       type: state.orderType,
-    //       status: UserOrderStatus.REJECTED,
-    //     ),
-    //     UserOrderListRoute(
-    //       type: state.orderType,
-    //       status: UserOrderStatus.CANCELED,
-    //     ),
-    //     UserOrderListRoute(
-    //       type: state.orderType,
-    //       status: UserOrderStatus.IN_PROGRESS,
-    //     ),
-    //     UserOrderListRoute(
-    //       type: state.orderType,
-    //       status: UserOrderStatus.ACCEPTED,
-    //     ),
-    //   ],
-    //   builder: (context, child, controller) {
-    //     return Scaffold(
-    //       appBar: AppBar(
-    //         actions: [
-    //           CustomTextButton(
-    //             text: state.orderType == OrderType.sell
-    //                 ? Strings.orderListTypeSell
-    //                 : Strings.orderListTypeBuy,
-    //             onPressed: () {
-    //               cubit(context).changeOrderType();
-    //             },
-    //           )
-    //         ],
-    //         leading: IconButton(
-    //           icon: Assets.images.icArrowLeft.svg(),
-    //           onPressed: () => context.router.pop(),
-    //         ),
-    //         elevation: 0.5,
-    //         backgroundColor: context.appBarColor,
-    //         centerTitle: true,
-    //         bottomOpacity: 1,
-    //         title: Strings.orderListTitle
-    //             .w(500)
-    //             .s(16)
-    //             .c(context.textPrimary),
-    //         bottom: TabBar(
-    //           isScrollable: true,
-    //           physics: BouncingScrollPhysics(),
-    //           indicator: MaterialIndicator(
-    //             height: 6,
-    //             tabPosition: TabPosition.bottom,
-    //             topLeftRadius: 100,
-    //             topRightRadius: 100,
-    //             color: Color(0xFF5C6AC3),
-    //             paintingStyle: PaintingStyle.fill,
-    //           ),
-    //           indicatorSize: TabBarIndicatorSize.label,
-    //           labelColor: Color(0xFF5C6AC3),
-    //           unselectedLabelColor: context.textSecondary,
-    //           indicatorColor: context.textPrimary,
-    //           controller: controller,
-    //           tabs: [
-    //             Tab(text: Strings.userOrderAll),
-    //             Tab(text: Strings.userOrderWait),
-    //             Tab(text: Strings.userOrderRejected),
-    //             Tab(text: Strings.userOrderCancelled),
-    //             Tab(text: Strings.userOrderInProgress),
-    //             Tab(text: Strings.userOrderAccepted)
-    //           ],
-    //         ),
-    //       ),
-    //       body: child,
-    //     );
-    //   },
-    // );
 
     return DefaultTabController(
       length: 2,
+      initialIndex: orderType == OrderType.buy ? 1 : 0,
       child: Builder(
         builder: (context) {
           final TabController tabController = DefaultTabController.of(context);
@@ -177,37 +98,49 @@ class UserOrdersPage
 
   AppBar _buildAppBar(BuildContext context, TabController controller) {
     return AppBar(
-      elevation: 0,
-      toolbarHeight: 116,
-      leadingWidth: 0,
-      leading: null,
+      toolbarHeight: 120,
       backgroundColor: context.appBarColor,
+      elevation: 0.5,
+      // centerTitle: true,
+      // title: Strings.orderListTitle.w(500).s(16)..c(context.textPrimary),
+      leading: IconButton(
+        onPressed: () => context.router.pop(),
+        icon: Assets.images.icArrowLeft.svg(color: Colors.transparent),
+      ),
       flexibleSpace: SafeArea(
         child: Column(
           children: [
             Row(
               children: [
-                Material(
-                  color: Colors.transparent,
+                SizedBox(
+                  width: 64,
+                  height: 64,
                   child: InkWell(
                     onTap: () {
                       HapticFeedback.lightImpact();
                       context.router.pop();
                     },
-                    borderRadius: BorderRadius.all(Radius.circular(12)),
-                    child: Assets.images.icArrowLeft.svg(),
+                    // borderRadius: BorderRadius.circular(12),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Assets.images.icArrowLeft.svg(),
+                    ),
                   ),
                 ),
                 SizedBox(width: 4),
-                Strings.orderListTitle
-                    .w(500)
-                    .s(16)
-                    .c(context.textPrimary)
-                    .copyWith(textAlign: TextAlign.center)
+                Expanded(
+                  child: Strings.orderListTitle
+                      .w(500)
+                      .s(16)
+                      .c(context.textPrimary)
+                      .copyWith(textAlign: TextAlign.center),
+                ),
+                SizedBox(width: 64),
               ],
             ),
+            SizedBox(height: 8),
             Container(
-              margin: EdgeInsets.fromLTRB(8, 0, 8, 0),
+              margin: EdgeInsets.fromLTRB(16, 0, 16, 0),
               height: 45,
               decoration: BoxDecoration(
                 color: context.backgroundGreyColor,
