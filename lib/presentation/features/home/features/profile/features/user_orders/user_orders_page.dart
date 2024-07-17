bffede7da5b8c6661fe6c472e97fd2c8143c8da8
kebrorba <abror.esonaliyev@gmail.com>
2024-07-17 15:19:37 +0500

    disabled: user orders page analogue variant code is commented

--- lib/presentation/features/home/features/profile/features/user_orders/user_orders_page.dart
+++ lib/presentation/features/home/features/profile/features/user_orders/user_orders_page.dart
@@ -8,7 +8,6 @@ import 'package:onlinebozor/core/gen/localization/strings.dart';
 import 'package:onlinebozor/domain/models/order/order_type.dart';
 import 'package:onlinebozor/domain/models/order/user_order_status.dart';
 import 'package:onlinebozor/presentation/features/home/features/profile/features/user_orders/features/user_order_list/user_order_list_page.dart';
-import 'package:onlinebozor/presentation/router/app_router.dart';
 import 'package:onlinebozor/presentation/support/cubit/base_page.dart';
 import 'package:onlinebozor/presentation/support/extensions/color_extension.dart';
 import 'package:tab_indicator_styler/tab_indicator_styler.dart';
@@ -174,136 +173,6 @@ class UserOrdersPage
     );
   }
 
-  /// analogue variant
-
-  Widget _buildBodyWithAutoTabsRouter(
-    UserOrdersState state,
-    TabController controller,
-  ) {
-    return AutoTabsRouter.tabBar(
-      physics: BouncingScrollPhysics(),
-      routes: _getAutoTabsRoutes(state.orderType),
-      builder: (context, childPage, controller) {
-        return Scaffold(
-          backgroundColor: context.cardColor,
-          appBar: _buildAutoTabsRouterAppBar(context, controller),
-          body: childPage,
-        );
-      },
-    );
-  }
-
-  List<PageRouteInfo<dynamic>> _getAutoTabsRoutes(OrderType orderType) {
-    Logger().w("user-orders _getRoutesByOrderType orderType = $orderType");
-    return [
-      UserOrderListRoute(
-        type: orderType,
-        status: UserOrderStatus.ALL,
-      ),
-      UserOrderListRoute(
-        type: orderType,
-        status: UserOrderStatus.WAIT,
-      ),
-      UserOrderListRoute(
-        type: orderType,
-        status: UserOrderStatus.REJECTED,
-      ),
-      UserOrderListRoute(
-        type: orderType,
-        status: UserOrderStatus.CANCELED,
-      ),
-      UserOrderListRoute(
-        type: orderType,
-        status: UserOrderStatus.IN_PROGRESS,
-      ),
-      UserOrderListRoute(
-        type: orderType,
-        status: UserOrderStatus.ACCEPTED,
-      ),
-    ];
-  }
-
-  AppBar _buildAutoTabsRouterAppBar(
-    BuildContext context,
-    TabController controller,
-  ) {
-    return AppBar(
-      toolbarHeight: 116,
-      backgroundColor: context.appBarColor,
-      elevation: 0.5,
-      leadingWidth: 0,
-      leading: Assets.images.icArrowLeft.svg(color: Colors.transparent),
-      flexibleSpace: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                SizedBox(
-                  width: 64,
-                  height: 64,
-                  child: InkWell(
-                    onTap: () {
-                      HapticFeedback.lightImpact();
-                      context.router.pop();
-                    },
-                    borderRadius: BorderRadius.circular(50),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Assets.images.icArrowLeft.svg(),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 4),
-                Expanded(
-                  child: Strings.orderListTitle
-                      .w(500)
-                      .s(16)
-                      .c(context.textPrimary)
-                      .copyWith(textAlign: TextAlign.center),
-                ),
-                SizedBox(width: 64),
-              ],
-            ),
-            SizedBox(height: 0),
-            _buildOrderTypeTabBar(context, controller),
-          ],
-        ),
-      ),
-      bottom: _buildAutoTabsRouterTabBar(context, controller),
-    );
-  }
-
-  TabBar _buildAutoTabsRouterTabBar(
-    BuildContext context,
-    TabController controller,
-  ) {
-    return TabBar(
-      isScrollable: true,
-      physics: BouncingScrollPhysics(),
-      indicator: MaterialIndicator(
-        height: 6,
-        tabPosition: TabPosition.bottom,
-        topLeftRadius: 100,
-        topRightRadius: 100,
-        color: Color(0xFF5C6AC3),
-        paintingStyle: PaintingStyle.fill,
-      ),
-      indicatorSize: TabBarIndicatorSize.label,
-      labelColor: Color(0xFF5C6AC3),
-      unselectedLabelColor: context.textSecondary,
-      indicatorColor: context.textPrimary,
-      controller: controller,
-      tabs: [
-        Tab(text: Strings.userOrderAll),
-        Tab(text: Strings.userOrderWait),
-        Tab(text: Strings.userOrderRejected),
-        Tab(text: Strings.userOrderCancelled),
-        Tab(text: Strings.userOrderInProgress),
-        Tab(text: Strings.userOrderAccepted)
-      ],
-    );
-  }
-
   /// common methods
 
   Container _buildOrderTypeTabBar(
@@ -334,4 +203,134 @@ class UserOrdersPage
       ),
     );
   }
+
+  /// analogue variant
+
+// Widget _buildBodyWithAutoTabsRouter(
+//   UserOrdersState state,
+//   TabController controller,
+// ) {
+//   return AutoTabsRouter.tabBar(
+//     physics: BouncingScrollPhysics(),
+//     routes: _getAutoTabsRoutes(state.orderType),
+//     builder: (context, childPage, controller) {
+//       return Scaffold(
+//         backgroundColor: context.cardColor,
+//         appBar: _buildAutoTabsRouterAppBar(context, controller),
+//         body: childPage,
+//       );
+//     },
+//   );
+// }
+
+// List<PageRouteInfo<dynamic>> _getAutoTabsRoutes(OrderType orderType) {
+//   Logger().w("user-orders _getRoutesByOrderType orderType = $orderType");
+//   return [
+//     UserOrderListRoute(
+//       type: orderType,
+//       status: UserOrderStatus.ALL,
+//     ),
+//     UserOrderListRoute(
+//       type: orderType,
+//       status: UserOrderStatus.WAIT,
+//     ),
+//     UserOrderListRoute(
+//       type: orderType,
+//       status: UserOrderStatus.REJECTED,
+//     ),
+//     UserOrderListRoute(
+//       type: orderType,
+//       status: UserOrderStatus.CANCELED,
+//     ),
+//     UserOrderListRoute(
+//       type: orderType,
+//       status: UserOrderStatus.IN_PROGRESS,
+//     ),
+//     UserOrderListRoute(
+//       type: orderType,
+//       status: UserOrderStatus.ACCEPTED,
+//     ),
+//   ];
+// }
+
+// AppBar _buildAutoTabsRouterAppBar(
+//   BuildContext context,
+//   TabController controller,
+// ) {
+//   return AppBar(
+//     toolbarHeight: 116,
+//     backgroundColor: context.appBarColor,
+//     elevation: 0.5,
+//     leadingWidth: 0,
+//     leading: Assets.images.icArrowLeft.svg(color: Colors.transparent),
+//     flexibleSpace: SafeArea(
+//       child: Column(
+//         children: [
+//           Row(
+//             children: [
+//               SizedBox(
+//                 width: 64,
+//                 height: 64,
+//                 child: InkWell(
+//                   onTap: () {
+//                     HapticFeedback.lightImpact();
+//                     context.router.pop();
+//                   },
+//                   borderRadius: BorderRadius.circular(50),
+//                   child: Padding(
+//                     padding: const EdgeInsets.all(20),
+//                     child: Assets.images.icArrowLeft.svg(),
+//                   ),
+//                 ),
+//               ),
+//               SizedBox(width: 4),
+//               Expanded(
+//                 child: Strings.orderListTitle
+//                     .w(500)
+//                     .s(16)
+//                     .c(context.textPrimary)
+//                     .copyWith(textAlign: TextAlign.center),
+//               ),
+//               SizedBox(width: 64),
+//             ],
+//           ),
+//           SizedBox(height: 0),
+//           _buildOrderTypeTabBar(context, controller),
+//         ],
+//       ),
+//     ),
+//     bottom: _buildAutoTabsRouterTabBar(context, controller),
+//   );
+// }
+
+// TabBar _buildAutoTabsRouterTabBar(
+//   BuildContext context,
+//   TabController controller,
+// ) {
+//   return TabBar(
+//     isScrollable: true,
+//     physics: BouncingScrollPhysics(),
+//     indicator: MaterialIndicator(
+//       height: 6,
+//       tabPosition: TabPosition.bottom,
+//       topLeftRadius: 100,
+//       topRightRadius: 100,
+//       color: Color(0xFF5C6AC3),
+//       paintingStyle: PaintingStyle.fill,
+//     ),
+//     indicatorSize: TabBarIndicatorSize.label,
+//     labelColor: Color(0xFF5C6AC3),
+//     unselectedLabelColor: context.textSecondary,
+//     indicatorColor: context.textPrimary,
+//     controller: controller,
+//     tabs: [
+//       Tab(text: Strings.userOrderAll),
+//       Tab(text: Strings.userOrderWait),
+//       Tab(text: Strings.userOrderRejected),
+//       Tab(text: Strings.userOrderCancelled),
+//       Tab(text: Strings.userOrderInProgress),
+//       Tab(text: Strings.userOrderAccepted)
+//     ],
+//   );
+// }
 }
