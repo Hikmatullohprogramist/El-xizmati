feda28236e8c6ad1ae3b720da36f8d92847d904f
kebrorba <abror.esonaliyev@gmail.com>
2024-07-30 11:32:26 +0500

    added: assets and translations

--- lib/core/gen/localization/strings.dart
+++ lib/core/gen/localization/strings.dart
@@ -12,7 +12,7 @@ part 'strings.g.dart';
 // ignore: unused_element
 @SheetLocalization(
   docId: '14H9f5eQWz2aH71NH9cGqEuy_BvgDyo_VMo-AlznFsfY',
-  version: 128,
+  version: 134,
   outDir: 'assets/localization',
   outName: 'translations.csv',
   preservedKeywords: [
