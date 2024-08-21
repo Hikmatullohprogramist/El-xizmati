2cf66551d2d31e1df7e9044678965b0d34d51622
Abduazim <qoribola5885@gmail.com>
2024-08-21 14:48:53 +0500

    set language tab controller

--- lib/presentation/features/common/set_intro_region/set_intro_region_page.dart
+++ lib/presentation/features/common/set_intro_region/set_intro_region_page.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:onlinebozor/core/extensions/text_extensions.dart';
@@ -13,7 +14,7 @@ import 'set_intro_region_cubit.dart';
 
 @RoutePage()
 class SetIntroRegionPage extends BasePage<SetIntroRegionCubit,
-    SetIntroRegionState, SetIntroRegionEvent> {
+    SetIntroRegionState, SetIntroRegionEvent>  {
   SetIntroRegionPage({super.key});
   @override
   void onEventEmitted(BuildContext context, SetIntroRegionEvent event) {
@@ -28,88 +29,99 @@ class SetIntroRegionPage extends BasePage<SetIntroRegionCubit,
   Widget onWidgetBuild(BuildContext context, SetIntroRegionState state) {
     return DefaultTabController(
       length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: context.backgroundWhiteColor,
-          elevation: 0,
-          centerTitle: true,
-          title: SizedBox(
-            width: 200,
-            child: TabBar(
-              labelPadding: EdgeInsets.all(0),
-              indicatorSize: TabBarIndicatorSize.label,
-              indicatorColor: Colors.transparent,
-              labelColor: context.colors.buttonPrimary,
-              indicator: BoxDecoration(
-                  color: context.colors.buttonPrimary,
-                  borderRadius: BorderRadius.all(Radius.circular(20))),
-              tabs: [
-                tab(context),
-                tab(context),
-                tab(context),
-              ],
-            ),
+      child: IntroTabbar(cubit: cubit(context),)
+    );
+  }
+}
+class IntroTabbar extends StatelessWidget {
+  const IntroTabbar({super.key, required this.cubit});
+  final SetIntroRegionCubit cubit;
+  @override
+  Widget build(BuildContext context) {
+    final controller = DefaultTabController.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: context.backgroundWhiteColor,
+        elevation: 0,
+        centerTitle: true,
+        title: SizedBox(
+          width: 200,
+          child: TabBar(
+            controller: controller,
+            labelPadding: EdgeInsets.all(0),
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorColor: Colors.transparent,
+            labelColor: context.colors.buttonPrimary,
+            indicator: BoxDecoration(
+                color: context.colors.buttonPrimary,
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+            tabs: [
+              tab(context),
+              tab(context),
+              tab(context),
+            ],
           ),
         ),
-        backgroundColor: context.backgroundWhiteColor,
-        resizeToAvoidBottomInset: false,
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.only(bottom: 88, top: 10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                    child: TabBarView(
-                  children: [
-                    intro(
-                        context,
-                        'El Xizmati tizimimizga xush kelibsiz!',
-                        'El Xizmati ishchilar va ish beruv-\nchilarni bir joyga ja’mlagan hamjamiyat platformasidir.',
-                        Assets.images.intro1),
-                    intro(
-                        context,
-                        'Endi ishingizni yaqin atrofdan toping!',
-                        'Kiritilgan lokatsiya orqali endilikda siz uyingizdan uzoq bo’lmagan hududlardan ish topasiz!',
-                        Assets.images.intro2),
-                    intro(
-                        context,
-                        'Barcha shaxsiy ma’lumotlaringiz ximoyalangan!',
-                        'Shaxsiy ma’lumotlaringiz, to’lovlaringiz, sizning ruxsatingizsiz hech kimga berilmaydi.',
-                        Assets.images.intro3),
-                  ],
-                )),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 35.0),
-                  child: CustomElevatedButton(
-                    text: 'Davom etish',
-                    onPressed: () {
-
-                    },
-                    rightIcon: Icon(
-                      Icons.arrow_forward,
-                      size: 18,
-                    ),
+      ),
+      backgroundColor: context.backgroundWhiteColor,
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(bottom: 88, top: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                  child: TabBarView(
+                    controller:controller,
+                    children: [
+                      intro(
+                          context,
+                          'El Xizmati tizimimizga xush kelibsiz!',
+                          'El Xizmati ishchilar va ish beruv-\nchilarni bir joyga ja’mlagan hamjamiyat platformasidir.',
+                          Assets.images.intro1),
+                      intro(
+                          context,
+                          'Endi ishingizni yaqin atrofdan toping!',
+                          'Kiritilgan lokatsiya orqali endilikda siz uyingizdan uzoq bo’lmagan hududlardan ish topasiz!',
+                          Assets.images.intro2),
+                      intro(
+                          context,
+                          'Barcha shaxsiy ma’lumotlaringiz ximoyalangan!',
+                          'Shaxsiy ma’lumotlaringiz, to’lovlaringiz, sizning ruxsatingizsiz hech kimga berilmaydi.',
+                          Assets.images.intro3),
+                    ],
+                  )),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 35.0),
+                child: CustomElevatedButton(
+                  text: 'Davom etish',
+                  onPressed: () {
+                    if(controller.index != controller.length)
+                    controller.animateTo(controller.index+1);
+                  },
+                  rightIcon: Icon(
+                    Icons.arrow_forward,
+                    size: 18,
                   ),
                 ),
-                SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 35.0),
-                  child: CustomOutlinedButton(
-                      text: "O'tkazib yuborish",
-                      onPressed: () {
-                        cubit(context).skipIntroRegion();
-                      }),
-                )
-              ],
-            ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 35.0),
+                child: CustomOutlinedButton(
+                    text: "O'tkazib yuborish",
+                    onPressed: () {
+                      cubit.skipIntroRegion();
+                    }),
+              )
+            ],
           ),
         ),
       ),
     );
   }
-
   Column intro(
       BuildContext context, String title, String subtitle, SvgGenImage svg) {
     return Column(
@@ -135,12 +147,11 @@ class SetIntroRegionPage extends BasePage<SetIntroRegionCubit,
       ],
     );
   }
-
   Container tab(BuildContext context) => Container(
-        width: MediaQuery.of(context).size.width * 0.1,
-        height: 8,
-        decoration: BoxDecoration(
-            color: Color(0xFF703EDB).withOpacity(0.3),
-            borderRadius: BorderRadius.circular(12)),
-      );
+    width: MediaQuery.of(context).size.width * 0.1,
+    height: 8,
+    decoration: BoxDecoration(
+        color: Color(0xFF703EDB).withOpacity(0.3),
+        borderRadius: BorderRadius.circular(12)),
+  );
 }
