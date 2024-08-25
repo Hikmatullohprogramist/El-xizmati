26a4b91093e24080ab98acb9fbe32629e16716ad
Abduazim <qoribola5885@gmail.com>
2024-08-25 14:26:48 +0500

    profile personal_page.dart

--- lib/presentation/features/home/features/my_profile/features/change_password/change_password_cubit.dart
+++ lib/presentation/features/home/features/my_profile/features/change_password/change_password_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:onlinebozor/presentation/support/cubit/base_cubit.dart';
+
+import '../../../../../../support/cubit/base_cubit.dart';
 part 'change_password_state.dart';
 part 'change_password_cubit.freezed.dart';
 class ChangePasswordCubit extends BaseCubit<ChangePasswordState, ChangePasswordEvent>{
--- lib/presentation/features/home/features/my_profile/features/change_password/change_password_page.dart
+++ lib/presentation/features/home/features/my_profile/features/change_password/change_password_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:onlinebozor/presentation/features/home/features/my_profile/features/change_password/change_password_cubit.dart';
-import 'package:onlinebozor/presentation/support/cubit/base_page.dart';
+
+import '../../../../../../support/cubit/base_page.dart';
+import 'change_password_cubit.dart';
 
 class ChangePasswordPage extends BasePage<ChangePasswordCubit, ChangePasswordState, ChangePasswordEvent>{
   @override
